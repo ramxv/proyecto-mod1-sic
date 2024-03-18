647b601b9ece6ab97cc97025eff1c939f60d024d
Dwight Sutherland <bobdwight17@gmail.com>
2024-03-18 15:01:57 -0500

    nombres de documentos
    Cambios de nombre de documentos derivados

--- Extract_data.py
+++ Extract_data.py
@@ -1,8 +1,8 @@
-# Proyecto de medio curso
-# Huella de carbono
+# Proyecto de medio curso para Samsung Innovation Campus: Huella de carbono
+
+# Lectura de datos y limpieza
+# Autor: Dwight Sutherland
 # Fecha: 3/18/2024
-
-
 
 
 
@@ -31,6 +31,7 @@ Los archivos creados por este código son:
 #   data: dataframe con los datos de 1990 en adelante
 #   data_ind: dataframe con los paises industrializados
 #   data_em: dataframe con los paises emergentes
+#   codebook_og: codebook original
 #   codebook: codebook con las categorias restantes
 
 
@@ -64,62 +65,72 @@ Los archivos creados por este código son:
 
 
 
+#________________________________________________________________________________________#
+#___________________________________________Codigo_______________________________________#
+#________________________________________________________________________________________#
 
-#___________________________________________Codigo_______________________________________
 
 
-
-
-# Importamos las librerias necesarias
+### Importamos las librerias necesarias
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Leemos el archivo de datos
-ruta = 'proyecto-mod1-sic'
-derivados = '\derivados'
-ruta_2 = ruta + derivados
+
+#___________________________________________Lectura de los datos_______________________________________
+
+### Leemos el archivo de datos
+ruta = 'proyecto-mod1-sic'      # Ruta donde se encuentra el archivo
+derivados = '\derivados'        # Ruta donde se guardaran los archivos creados
+ruta_2 = ruta + derivados       # Ruta completa
 data_og = ruta + '\owid-co2-data.csv'
-data_og = pd.read_csv(data_og)
+data_og = pd.read_csv(data_og)      # Dataframe con los datos originales
+
+
 
 #___________________________________________Dataframe con los datos de 1990 en adelante_______________________________________
+### Eliminamos las filas correspondientes a 1989 para atrás y guardamos en otro archivo nuevo
 
-# Eliminamos las filas correspondientes a 1989 para atrás y guardamos en otro archivo nuevo
-data = data_og[data_og['year'] > 1989]
-data.to_csv(ruta_2 + '\owid-co2-data-1990.csv', index=False)
+data = data_og[data_og['year'] > 1989]      # Dataframe con los datos de 1990 en adelante
+data.to_csv(ruta_2 + '\data-1990.csv', index=False)
 
 
 
 #___________________________________________Dataframe con los paises que nos interesan_______________________________________
+'''
+Creamos un nuevo dataframe con los paises que nos interesan
+United States, Canada, Japan, Germany, United Kingdom, China, India, Mexico, South Korea
+'''
 
-
-# Creamos un nuevo dataframe con los paises que nos interesan
-# United States, Canada, Japan, Germany, United Kingdom, China, India, Mexico, South Korea
 data = data[(data['country'] == 'United States') | (data['country'] == 'Canada') | (data['country'] == 'Japan')|
             (data['country'] == 'Germany') | (data['country'] == 'United Kingdom') | (data['country'] == 'China')|
             (data['country'] == 'India') | (data['country'] == 'Mexico') | (data['country'] == 'South Korea')]
 
 
-# Eliminamos las categorias con valores nulos
+### Eliminamos las categorias con valores nulos
 data_columns = data.columns
-data = data.dropna(how='any', axis=1)
+data = data.dropna(how='any', axis=1)       # Eliminamos las categorias con valores nulos
 data_columns_new = data.columns
-data.to_csv(ruta_2 + '\owid-co2-data-1990-country.csv', index=False)
+data.to_csv(ruta_2 + '\data-1990-country_of_interest.csv', index=False)
 
-print(f'Las categorias eliminadas son: {set(data_columns) - set(data_columns_new)}\n')
-print(f'Las categorias que permanecen son: {set(data_columns_new)}\n')
+'''print(f'Las categorias eliminadas son: {set(data_columns) - set(data_columns_new)}\n')
+print(f'Las categorias que permanecen son: {set(data_columns_new)}\n')'''
 
 
 
 # ___________________________________________Dataframe con los paises industrializados y emergentes_______________________________________
 
-# Separo los datos en dos grupos
-# Paises industrializados: United States, Canada, Japan, Germany, United Kingdom
-# Paises emergentes: China, India, Mexico, South Korea
+'''
+Separo los datos en dos grupos:
+    - Paises industrializados: United States, Canada, Japan, Germany, United Kingdom
+    - Paises emergentes: China, India, Mexico, South Korea
+'''
 
+### Dataframe con los paises industrializados
 data_ind = data[(data['country'] == 'United States') | (data['country'] == 'Canada') | (data['country'] == 'Japan')
                 | (data['country'] == 'Germany') | (data['country'] == 'United Kingdom')]
 
+### Dataframe con los paises emergentes
 data_em = data[(data['country'] == 'China') | (data['country'] == 'India') | (data['country'] == 'Mexico')
                 | (data['country'] == 'South Korea')]
 
@@ -131,12 +142,9 @@ data_em.to_csv(ruta_2 + '\emergentes.csv', index=False)
 
 #___________________________________________Codebook con las categorias restantes_______________________________________
 
-# Modificacion del codebook para que permanezcan solo las categorias restantes
-codebook = pd.read_csv(ruta + '\owid-co2-codebook.csv') 
-codebook = codebook[codebook['column'].isin(data_columns_new)]
-codebook.to_csv(ruta_2 + '\owid-co2-codebook-new.csv', index=False)
+### Modificacion del codebook para que permanezcan solo las categorias restantes
+codebook_og = pd.read_csv(ruta + '\owid-co2-codebook.csv')     
+codebook = codebook_og[codebook_og['column'].isin(data_columns_new)]      
+codebook.to_csv(ruta_2 + '\codebook-new.csv', index=False)
 
-print(f'El nuevo codebook tiene {len(codebook)} categorias\nEL codebook original tiene {len(data_columns)} categorias\n')
-
-
-
+# print(f'El nuevo codebook tiene {len(codebook)} categorias\nEL codebook original tiene {len(data_columns)} categorias\n')
